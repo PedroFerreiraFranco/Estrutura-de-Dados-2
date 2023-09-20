02677d3137a1a303451c7ed03ef2279005f28bb6
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-09-19 22:36:26 -0300

    Update BubbleSort.java

--- Revisão1/src/revisao1/BubbleSort.java
+++ Revisão1/src/revisao1/BubbleSort.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package revisao1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author luizh
- */
 public class BubbleSort {
     
     public static long compara = 0, trocas = 0;
