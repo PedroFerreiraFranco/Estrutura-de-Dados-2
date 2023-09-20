f0c70ad8f43ef85d9e3f3dae44af0e9f0b4dc00d
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-09-19 22:36:43 -0300

    Update JavaSorts.java

--- Revisão1/src/revisao1/JavaSorts.java
+++ Revisão1/src/revisao1/JavaSorts.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package revisao1;
 
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author luizh
- */
 public class JavaSorts {
     
     public static int menuEntrada() {
